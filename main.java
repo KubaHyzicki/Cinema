c2e3b8ca360600cd00a08baf13fa9c1454f5494c
KubaHyzicki <kuba.hyzicki@gmail.com>
2020-01-09 22:17:40 +0100

    call bookTicket

--- main.java
+++ main.java
@@ -2,7 +2,8 @@ import Application.*;
 import Application.Users.Customer;
 import Application.Users.LoginDetails;
 
-import java.util.Date;
+import java.util.*;
+import java.sql.Time;
 
 class App {
 	public static void main(String[] args){
@@ -12,8 +13,8 @@ class App {
 		cinema.setUser(customer);
 		Movie movie = new Movie("Pokahontas","Czy wiesz czemu wilk tak wyje w księżycooową noc? Bo było ciemno i się w mały palec walnąąął...");
 		Date screeningDate = new Date( 2020, 2, 10);
-		// Time time
-		// cinema.bookTicket(customer, movie, screeningDate, Time time);
+		Time time = new Time(13, 13, 13);
+		cinema.bookTicket(customer, movie, screeningDate, time);
 		// cinema.run();
 	}
 }

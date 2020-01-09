350ae457af271b00f041af54618a7759f95f0c89
KubaHyzicki <kuba.hyzicki@gmail.com>
2020-01-09 22:30:09 +0100

    movie constructor

--- main.java
+++ main.java
@@ -12,7 +12,7 @@ class App {
 		Customer customer = new Customer(new LoginDetails("jakubfaldasz", "passw0rd", "jakubfaldasz@gmail.com"), new Date(1997, 5,13));
 		cinema.setUser(customer);
 		Movie movie = new Movie("Pokahontas","Czy wiesz czemu wilk tak wyje w księżycooową noc? Bo było ciemno i się w mały palec walnąąął...");
-		Date screeningDate = new Date( 2020, 2, 10);
+		Date screeningDate = new Date(2020, 2, 10);
 		Time time = new Time(13, 13, 13);
 		cinema.bookTicket(customer, movie, screeningDate, time);
 		// cinema.run();
